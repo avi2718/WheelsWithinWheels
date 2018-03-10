28e6169eb72c40d95480fdb68394ef34314ec0a3
eshkolnik1 <36490296+eshkolnik1@users.noreply.github.com>
2018-03-09 19:53:12 -0500

    Updated some variables

--- src/wheelswithinwheels/CustomerDatabase.java
+++ src/wheelswithinwheels/CustomerDatabase.java
@@ -1,25 +1,25 @@
-package wheelswithinwheels;
-
-import java.util.Map;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package wheelswithinwheels;
 
 /**
  *
- * @author bhairavi
+ * @author Shai
  */
+
+import java.util.Map;
+
 public class CustomerDatabase {
-    public Map<Integer, Customer> customers;
+    public static Map<Integer, Customer> customers;
     
-    public Customer findByNumber(int customerNumber) {
+    public static Customer findByNumber(int customerNumber) {
         return customers.get(customerNumber);
     }
     
-    public Customer findByName(String customerFirstName, String customerLastName) {
+    public static Customer findByName(String customerFirstName, String customerLastName) {
         for (Customer c : customers.values()) {
             if (c.firstName == customerFirstName && c.lastName == customerLastName) {
                 return c;
