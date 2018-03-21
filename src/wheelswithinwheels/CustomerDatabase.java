97ac237eeeeb8f6221feed8b0c1366649e0a97e8
maxwelllittle <maxwelllittle@172.26.18.14>
2018-03-20 20:45:59 -0400

    Added printcname

--- src/wheelswithinwheels/CustomerDatabase.java
+++ src/wheelswithinwheels/CustomerDatabase.java
@@ -9,47 +9,57 @@ package wheelswithinwheels;
  *
  * @author Shai
  */
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeSet;
 
 public class CustomerDatabase {
+
     public static Map<Integer, Customer> customers = new HashMap<Integer, Customer>();
-    
+
     public static Customer findByNumber(int customerNumber) {
         return customers.get(customerNumber);
     }
-    
-    public static Customer findByName(String customerFirstName, String customerLastName) {
+
+    public static Customer findByName(String customerLastName) {
         for (Customer c : customers.values()) {
-            if (c.firstName == customerFirstName && c.lastName == customerLastName) {
+            if (c.lastName == customerLastName) {
                 return c;
             }
         }
         return null;
     }
-    
+
     public void printcnum() {
-        
         TreeSet<Integer> orderedSet = new TreeSet<Integer>();
         Iterator<Integer> iter = customers.keySet().iterator();
-        while(iter.hasNext()){//Places Keys in a treeset which automatically sorts them, then prints the customers using that set
+        while (iter.hasNext()) {//Places Keys in a treeset which automatically sorts them, then prints the customers using that set
             orderedSet.add(iter.next());
-     }
-        
+        }
+
         Iterator<Integer> treeSetIter = orderedSet.iterator();
         while (treeSetIter.hasNext()) {
             System.out.println(customers.get(treeSetIter.next()));
         }
     }
-    
+
+    public void printcname() {
+        TreeSet<String> orderedSet = new TreeSet<String>();
+        for (Customer c : customers.values()) {//Places values in a treeset which automatically sorts them, then prints that set
+            orderedSet.add(c.lastName);
+        }
+        Iterator<String> treeSetIter = orderedSet.iterator();
+        while (treeSetIter.hasNext()) {
+            System.out.println(findByName(treeSetIter.next()));
+        }
+    }
+
     public void addCustomer(String firstName, String lastName) {
         Customer c = new Customer(firstName, lastName);
         customers.put(c.customerNumber, c);
     }
-    
+
     public boolean isValidCustomerID(String customerID) {
         try {
             return customers.containsKey(Integer.parseInt(customerID));
@@ -57,14 +67,14 @@ public class CustomerDatabase {
             return false;
         }
     }
-    
+
     public void printCustomers() {
-        System.out.println("CUSTOMER NUMBER" + "\t\tCUSTOMER NAME"); 
+        System.out.println("CUSTOMER NUMBER" + "\t\tCUSTOMER NAME");
         for (Customer c : customers.values()) {
             System.out.println(c);
         }
     }
-    
+
     public String savableString() {
         String s = "";
         for (Customer c : customers.values()) {
